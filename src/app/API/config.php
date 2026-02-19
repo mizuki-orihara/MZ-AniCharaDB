74c1281c3ccfa86e9ce9102449c4e754dd32e472
Mizuki <mizukiorihara@gmail.com>
2026-02-19 19:59:20 +0900

    仮組み。

--- src/app/API/config.php
+++ src/app/API/config.php
@@ -1,16 +1,14 @@
 ﻿<?php
-// config.php — 構造定義（パス・スキーマ・固定値）
+// config.php ? 構造定義（パス・スキーマ・固定値）
 
-// ベースディレクトリ（このファイルの位置を基点に一元管理）
-define('BASE_DIR',        realpath (__DIR__ .'/MZ-AniCharaDB/src/app'));
-define('import_norm_DIR', BASE_DIR .'/01a_import_norm');
-define('import_bulk_DIR',  BASE_DIR .'/01b_import_bulk');
-define('varid_DIR',       BASE_DIR .'/02_varid');
-define('dispatch_DIR',     BASE_DIR .'/03a_dispatch');
-define('marge_DIR',        BASE_DIR .'/03b_marge');
-define('mainDB_DIR',      BASE_DIR .'/MainDB' ); 
-
-
+// ベースディレクトリ（APIフォルダから見たappフォルダの絶対パスを取得）
+define('BASE_DIR',        realpath(__DIR__ . '/..'));
+define('import_norm_DIR', BASE_DIR . '/01a_import_norm');
+define('import_bulk_DIR', BASE_DIR . '/01b_import_bulk');
+define('valid_DIR',       BASE_DIR . '/02_valid');
+define('dispatch_DIR',    BASE_DIR . '/03a_dispatch');
+define('merge_DIR',       BASE_DIR . '/03b_merge');
+define('mainDB_DIR',      BASE_DIR . '/mainDB');
 
 
 
